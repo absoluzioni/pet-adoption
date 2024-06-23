b8aa2d04f4c52afa3f745080b73a5203470a3554
Andrea <absoluzioni@gmail.com>
2024-06-23 21:49:31 +0200

    Added Filter task

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,18 @@
 const template = document.querySelector("#pet-card-template");
 const wrapper = document.createDocumentFragment();
+const filterButtons = document.querySelectorAll(".filter-list button");
+let petsList = document.querySelector(".bottom-section__list");
+
+filterButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    filterButtons.forEach(btn => {
+      btn.classList.remove("active");
+    });
+    button.classList.add("active");
+    petsList.innerHTML = "";
+    petsArea();
+  });
+});
 
 async function getTemperatureOfMiami() {
   const weatherPromise = await fetch(
@@ -10,8 +23,14 @@ async function getTemperatureOfMiami() {
     (((weather.properties.periods[0].temperature - 32) * 5) / 9).toFixed(1)
   );
 }
-
 getTemperatureOfMiami();
+
+/**
+ * Calculates the age of a pet based on the provided birth year and returns a formatted age text.
+ *
+ * @param {number} petage - The birth year of the pet
+ * @return {string} The formatted age text of the pet
+ */
 function createAgeText(petage) {
   const currentYear = new Date().getFullYear();
   const age = currentYear - petage;
@@ -20,6 +39,9 @@ function createAgeText(petage) {
   return ageText;
 }
 async function petsArea() {
+  const activeButton = document.querySelector(".filter-list .active");
+  console.log(activeButton);
+
   const petsPromise = await fetch(
     "https://learnwebcode.github.io/bootcamp-pet-data/pets.json"
   );
@@ -27,11 +49,24 @@ async function petsArea() {
   pets.forEach(pet => {
     const clone = template.content.cloneNode(true);
 
+    clone.querySelector(".bottom-section__item").dataset.species = pet.species;
+
+    if (
+      pet.species == activeButton.dataset.filter ||
+      activeButton.dataset.filter == "all"
+    ) {
+      clone.querySelector(".bottom-section__item").style.display = "grid";
+    } else {
+      clone.querySelector(".bottom-section__item").style.display = "none";
+    }
+
     clone.querySelector(".item__name").textContent = pet.name;
     clone.querySelector(".item__description").textContent = pet.description;
     clone.querySelector(".item__age").textContent = createAgeText(
       pet.birthYear
     );
+
+    if (!pet.photo) pet.photo = "images/fallback.jpg";
     clone.querySelector(".item__image img").setAttribute("src", pet.photo);
     clone
       .querySelector(".item__image img")
@@ -40,7 +75,6 @@ async function petsArea() {
     wrapper.appendChild(clone);
   });
 
-  document.querySelector(".bottom-section__list").appendChild(wrapper);
+  petsList.appendChild(wrapper);
 }
-
 petsArea();
